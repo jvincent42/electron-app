36782b7555144daebc3ec7b8f6a4f269880d576a
ato <jvincent@student.42.fr>
2016-07-06 01:44:02 +0200

    utils

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,11 @@
-export const lift = (a) => (() => a)
+/* ----- Utils ----------------------------------------------------------- */
+import React from 'react'
+
+export const lift = x => (() => x)
+
+export const identity = x => x
+
+export const mapC = (Component, xs) => props =>
+  <div className={props.className}>
+    { xs.map((x, i) => <Component key={i} {...x} {...props} />) }
+  </div>
